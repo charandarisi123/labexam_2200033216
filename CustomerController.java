55aa65b55f32be6e57c4e662537ea498a393d7f2
charandarisi123 <162859573+charandarisi123@users.noreply.github.com>
2024-12-07 13:09:43 +0530

    Add files via upload

---
+++ CustomerController.java
@@ -0,0 +1,30 @@
+package com.klef.jfsd.exam.controller;
+
+import com.klef.jfsd.exam.model.Customer;
+import com.klef.jfsd.exam.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/customers")
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable Long id, @RequestBody Customer updatedCustomer) {
+        Customer customer = customerService.updateCustomer(id, updatedCustomer);
+        if (customer != null) {
+            return ResponseEntity.ok(customer);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
